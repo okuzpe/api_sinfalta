6d97725317567bc36a32989aff703ea8dcdfa9df
omarpe19 <omar_kuzma14@hotmail.com>
2017-03-28 20:26:16 -0400

    make it better

---
+++ config/database.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+'connections' => [
+
+        /*'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ],*/
+
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => env('DB_DATABASE', base_path('database/database.sqlite')),
+            'prefix'   => env('DB_PREFIX', ''),
+        ],
+
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', 3306),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => env('DB_CHARSET', 'utf8'),
+            'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+            'prefix'    => env('DB_PREFIX', ''),
+            'timezone'  => env('DB_TIMEZONE', '+00:00'),
+            'strict'    => env('DB_STRICT_MODE', false),
+        ],
+
+]
+];
