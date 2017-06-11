f7f781ed34c465d6d17f257cf99d38476ada680d
omarpe19 <omar_kuzma14@hotmail.com>
2017-06-10 20:47:58 -0400

    make it better

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -36,8 +36,8 @@ class PerfilController extends Controller
             return response()->json(['success' => false]);
         } else {
 
-
-            $URL_PERFIL= Cloudder::secureShow('fotoPerfil'.$show[0]->id_jugador,array ("width" => 250, "height" => 250));
+            //$URL_PERFIL= Cloudder::secureShow('fotoPerfil'.$show[0]->id_jugador,array ("width" => 250, "height" => 250));
+            $URL_PERFIL="https://res.cloudinary.com/hmb2xri8f/image/upload/fotoPerfil".$show[0]->id_jugado;
             return response()->json(['datos' => $show, 'success' => true,'url_perfil'=>$URL_PERFIL]);//,'url_perfil'=>$URL_PERFIL]
         }
     }
