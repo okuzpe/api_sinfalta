88dcb9b7c1263c59b205024fb69de6bbacc7d6d7
omar <omar_kuzma14@hotmail.com>
2017-08-25 10:41:26 -0400

    make it better

--- app/Http/Controllers/CargaController.php
+++ app/Http/Controllers/CargaController.php
@@ -21,19 +21,19 @@ class CargaController extends Controller
     public function index(Request $request){
         $id_jugador = DB::table('jugador')
             ->where( 'api_token','=',Input::get('api_token'))
-            ->select('id_jugador')
-            ->first()->id_jugador;
+            ->select('id_jugador','tiene_imagen')
+            ->get();
 
         $tiene =DB::table('jugador_equipo')
-            ->where( 'id_jugador','=',$id_jugador)
+            ->where( 'id_jugador','=',$id_jugador[0]->id_jugador)
             ->select('id_jugador','id_equipo','id_rangoequipo')
             ->get();
 
         if (!$tiene->isEmpty()){
-            return response()->json(['success' => true,'tiene_equipo'=>true,'img_url'=>"https://res.cloudinary.com/hmb2xri8f/image/upload/fotoPerfil".$id_jugador]);
+            return response()->json(['success' => true,'tiene_equipo'=>true,'img_url'=>"https://res.cloudinary.com/hmb2xri8f/image/upload/fotoPerfil".$id_jugador[0]->id_jugador,'tiene_imagen'=>$id_jugador[0]->tiene_imagen]);
 
         }else{
-            return response()->json(['success' => true,'tiene_equipo'=>false,'img_url'=>"https://res.cloudinary.com/hmb2xri8f/image/upload/fotoPerfil".$id_jugador]);
+            return response()->json(['success' => true,'tiene_equipo'=>false,'img_url'=>"https://res.cloudinary.com/hmb2xri8f/image/upload/fotoPerfil".$id_jugador[0]->id_jugador,'tiene_imagen'=>$id_jugador[0]->tiene_imagen]);
         }
 
 
