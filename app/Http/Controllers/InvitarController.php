4925b564fdde78067082419c27dd0055c649ff0b
omar <omar_kuzma14@hotmail.com>
2017-10-08 16:43:18 -0400

    make it better

--- app/Http/Controllers/InvitarController.php
+++ app/Http/Controllers/InvitarController.php
@@ -26,34 +26,40 @@ class InvitarController extends Controller
 
     public function invitarAEquipo(Request $request)
     {
-//
-//        $id_jugador = DB::table('jugador')
-//            ->select('id_jugador')
-//            ->where('api_token', '=', $request->get('api_token'))
-//            ->first();
-//
-//        $existe_invitacion=Invitaciones::where('id_invitador', '=', $id_jugador->id_jugador)
-//            ->where('id_invitado', '=', Input::get('id_invitado'))
-//            ->where('id_equipo', '=', Input::get('id_equipo'))
-//            ->first();
-//
-//
-//
-//        if (!is_null($existe_invitacion)){
-//            return response()->json(['success' => true,"estado"=>"Ya se ha invitado a este jugador al equipo."]);
-//        }else{
-//            $invitacion= new Invitaciones();
-//            $invitacion->id_invitador= $id_jugador->id_jugador;
-//            $invitacion->id_invitado = $request->get('id_invitado');
-//            $invitacion->id_equipo=$request->get('id_equipo');
-//            if ($invitacion->save()){
-//                return response()->json(['success' => true,"estado"=>"Jugador invitado."]);
-//            }else{
-//                return response()->json(['success' => false,"estado"=>"Error inesperado."]);
-//            }
-//        }
 
-        return response()->json(['success' => true,"estado"=>$request->get('id_equipo')]);
+        $id_jugador = DB::table('jugador')
+            ->join('jugador_equipo', 'jugador_equipo.id_jugador', '=', 'jugador.id_jugador')
+            ->select('jugador.id_jugador','jugador_equipo.id_rangoequipo')
+            ->where('jugador.api_token', '=', $request->get('api_token'))
+            ->first();
+
+        $existe_invitacion=Invitaciones::where('id_invitador', '=', $id_jugador->id_jugador)
+            ->where('id_invitado', '=', Input::get('id_invitado'))
+            ->where('id_equipo', '=', Input::get('id_equipo'))
+            ->first();
+
+
+        if ($id_jugador->id_rangoequipo==1 or $id_jugador->id_rangoequipo==2){
+
+
+            if (!is_null($existe_invitacion)){
+                return response()->json(['success' => true,"estado"=>"Ya se ha invitado a este jugador al equipo."]);
+            }else{
+                $invitacion= new Invitaciones();
+                $invitacion->id_invitador= $id_jugador->id_jugador;
+                $invitacion->id_invitado = $request->get('id_invitado');
+                $invitacion->id_equipo=$request->get('id_equipo');
+                if ($invitacion->save()){
+                    return response()->json(['success' => true,"estado"=>"Jugador invitado."]);
+                }else{
+                    return response()->json(['success' => false,"estado"=>"Error inesperado"]);
+                }
+            }
+
+        }else{
+            return response()->json(['success' => false,"estado"=>"No tienes los privilegios para invitar"]);
+        }
+
     }
 
 
