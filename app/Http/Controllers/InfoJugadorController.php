bf45dbcc4f0d8c5b0e97eeede81c9d92198c6170
omar <omar_kuzma14@hotmail.com>
2017-12-13 05:21:56 -0400

    make it better

--- app/Http/Controllers/InfoJugadorController.php
+++ app/Http/Controllers/InfoJugadorController.php
@@ -29,6 +29,7 @@ class InfoJugadorController extends Controller
     public function amigosMostrar(Request $request)
     {
         $token=$request->get('api_token');
+
         $id_jugador = DB::table('jugador')
             ->select('id_jugador')
             ->where('api_token', '=', $token)
@@ -37,15 +38,19 @@ class InfoJugadorController extends Controller
 //        $amigos=DB::table('amigos')
 //            ->join('jugador','jugador.id_jugador','=','amigos.id_jugador')
 //            ->where('jugador.api_token','=',$token)
-//            ->select('amigos.id_amigo','amigos.id_jugador')
+//            ->select('amigos.id_amigo')
 //            ->get();
 
-
         $amigos=DB::table('amigos')
-            ->where('id_amigo','=',$id_jugador->id_jugador)
-            ->orwhere('id_jugador','=',$id_jugador->id_jugador)
             ->select('id_amigo','id_jugador')
+            ->orwhere('id_jugador','=',$id_jugador->id_jugador)
+            ->orwhere('id_amigo','=',$id_jugador->id_jugador)
             ->get();
+
+        if ($amigos[0]->id_amigo==$id_jugador->id_jugador){
+            $amigos[0]->id_amigo=$amigos[0]->id_jugador;
+
+        }
 
         $i = 0;
         foreach ($amigos as $a) {
@@ -60,8 +65,6 @@ class InfoJugadorController extends Controller
 
 
         return response()->json(['success'=>true,'amigos'=>$amigos]);
-
-//        return response()->json(['success'=>true,'amigos'=>$amigos]);
 
     }
 }
