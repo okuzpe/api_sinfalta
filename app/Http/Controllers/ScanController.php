0f3364098cba828ec2474f342b04d928f7f4aa5f
omar <omar_kuzma14@hotmail.com>
2018-05-22 22:22:18 -0400

    make it better

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -22,33 +22,33 @@ class ScanController extends Controller
     public function retar(Request $request)
     {
         $api_token = $request->get('api_token');
-        $nombre_retador=$request->get('nombre');
-        $id_equipo_retar=$request->get('id_equipo_retar');
+        $nombre_retador = $request->get('nombre');
+        $id_equipo_retar = $request->get('id_equipo_retar');
 
-        $id_jugador_creador= DB::table('jugador')
+        $id_jugador_creador = DB::table('jugador')
             ->select('id_jugador')
             ->where('api_token', '=', $api_token)
             ->first()->id_jugador;
 
-        $equipo=DB::table('equipo')
-            ->select('id_equipo','id_tipoequipo')
+        $equipo = DB::table('equipo')
+            ->select('id_equipo', 'id_tipoequipo')
             ->where('nombre', '=', $nombre_retador)
             ->first();
 
-        $jugador_id_jugador=DB::table('jugador_equipo')
+        $jugador_id_jugador = DB::table('jugador_equipo')
             ->select('id_jugador')
             ->where('id_rangoequipo', '=', $id_jugador_creador)
             ->where('id_equipo', '=', $equipo->id_equipo)
             ->first();
 
-        $date =date_create($request->get('fechahora_inico'));
+        $date = date_create($request->get('fechahora_inico'));
         $date->format('Y-m-d H:i:s');
 
         $partida = new Partida();
 
         $partida->id_estatus = 1;
         $partida->id_creador = $id_jugador_creador;
-        $partida->id_tipopartida=$equipo->id_tipoequipo;
+        $partida->id_tipopartida = $equipo->id_tipoequipo;
         $partida->longitud = $request->get('lon');
         $partida->latitud = $request->get('lat');
         $partida->descripcion = $request->get('descripcion');
@@ -56,48 +56,53 @@ class ScanController extends Controller
         $partida->equipo_creador = $equipo->id_equipo;
 
 
-        $existe_partida=DB::table('partida')
+        $existe_partida = DB::table('partida')
             ->where('equipo_creador', '=', $equipo->id_equipo)
             ->where('equipo_retador', '=', $id_equipo_retar)
             ->count();
 
-
-        $cantidad_partidas_creadas=DB::table('partida')
+        $cantidad_partidas_creadas = DB::table('partida')
             ->where('equipo_creador', '=', $equipo->id_equipo)
             ->where('id_estatus', '=', '1')
             ->count();
 
-        //BUG DE LA CONDICION...
-        if($existe_partida<1) {
-//            $existe_partida=null;
-            if ($equipo->id_equipo != $id_equipo_retar) {
-                if ($cantidad_partidas_creadas < 3) {
-//                    if ($partida->save()) {
+        $existe_notificacion = DB::table('notificaciones')
+            ->where('id_partida', '=', $partida->id_partida)
+            ->where('id_creador', '=', $equipo->id_equipo)
+            ->where('id_destino', '=', $jugador_id_jugador->id_jugador)
+            ->where('id_equipo', '=', $id_equipo_retar)
+            ->exist();
 
+        //BUG DE LA CONDICION...
+        if ($existe_notificacion){
+            if ($existe_partida < 1) {
+                if ($equipo->id_equipo != $id_equipo_retar) {
+                    if ($cantidad_partidas_creadas < 3) {
                         $notificacion = new Notificaion();
-                        $notificacion->id_partida=$partida->id_partida;
+                        $notificacion->id_partida = $partida->id_partida;
                         $notificacion->id_creador = $equipo->id_equipo; //fino-> id_equipo a creador de la partida
                         $notificacion->id_destino = $jugador_id_jugador->id_jugador;//fino-> id del capitan de equipo
                         $notificacion->id_equipo = $id_equipo_retar;//fino-> id_equipo a retar en la partida
                         $notificacion->id_tipo_notificacion = 3;
                         $notificacion->id_estatus = 3;
 
-                        if ($notificacion->save() && $partida->save()) {
+                        if ($partida->save() && $notificacion->save()) {
                             return response()->json(['success' => true, "estado" => "Solicitud de reto de partida enviada"]);
                         } else {
                             return response()->json(['success' => false, "estado" => "No se retar al equipo"]);
                         }
-//                    } else {
-//                        return response()->json(['success' => false, "estado" => "No se retar al equipo"]);
-//                    }
+                    } else {
+                        return response()->json(['success' => false, "estado" => "No se retar al equipo, no se puede tener mas de 3 partidas por equipo creadas al mismo tiempo"]);
+                    }
                 } else {
-                    return response()->json(['success' => false, "estado" => "No se retar al equipo, no se puede tener mas de 3 partidas por equipo creadas al mismo tiempo"]);
+                    return response()->json(['success' => false, "estado" => "No puedes retar a tu mismo equipo"]);
                 }
             } else {
-                return response()->json(['success' => false, "estado" => "No puedes retar a tu mismo equipo"]);
+                return response()->json(['success' => false, "estado" => "Ya se envio la solicitud de reto de partida"]);
             }
         }else{
-            return response()->json(['success' => false, "estado" => "Ya se envio la solicitud de reto de partida"]);
+            return response()->json(['success' => false, "estado" => "La notificacion ya ha sido enviada,favor espere"]);
+
         }
     }
 
